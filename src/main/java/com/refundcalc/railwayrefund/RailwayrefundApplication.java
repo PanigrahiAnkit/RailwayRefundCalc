4410d22a915786e68b09bcad48395aa72a2e74c8
Ankit Panigrahi <panigrahi0702@gmail.com>
2025-05-27 19:29:13 +0530

    first commit

---
+++ src/main/java/com/refundcalc/railwayrefund/RailwayrefundApplication.java
@@ -0,0 +1,13 @@
+package com.refundcalc.railwayrefund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwayrefundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RailwayrefundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/refundcalc/railwayrefund/RailwayrefundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.refundcalc.railwayrefund;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailwayrefundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
